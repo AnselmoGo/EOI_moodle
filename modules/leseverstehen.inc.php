988cc3aea43b60b718b5dec703fbd0f8adee4439
Anselmo Gomez <anselmo.eoi@gmail.com>
2019-09-20 15:21:21 +0200

    change leseverstehen

--- modules/leseverstehen.inc.php
+++ modules/leseverstehen.inc.php
@@ -9,23 +9,27 @@
 	$mysqli->set_charset("utf8");
 ?>
 	<div class="row justify-content-center mt-5">
-		<div class="justify-content-center mt-3 col-4">
-			<h2>Lückentext</h2>
+		<div class="center mt-3 col-4">
+			<h1>Lückentext</h1>
 		</div>
-		<div class="justify-content-center mt-3 mb-5 col-10">
+		<div class="center mt-3 mb-5 col-10">
 			In diesem Text musst du entscheiden welches von den Wörtern in den Kästen zu dem Satz passt. Viel Glück!!
 			<br />
 			Vorsicht, es gibt 5 Wörter zu viel!!
 		</div>
 	</div>
+	
+	<div class="row justify-content-center">
+		<div class="my-3 col-10 line_high">							
+			<?php
+				$table = 'lv_select';
+				$tsbId = 1;
 
-<?php
-	$table = 'lv_select';
-	$tsbId = 1;
+				$text_builder = new build_Text($table);
+				$text_builder->set_TbsId($tsbId);
+				$text = $text_builder->get_Text($mysqli);
+				echo $text;
 
-	$text_builder = new build_Text($table);
-	$text_builder->set_TbsId($tsbId);
-	$text = $text_builder->get_Text($mysqli);
-	echo $text;
-
-?>
+			?>
+		</div>
+	</div>
--- php/build_Text.class.php
+++ php/build_Text.class.php
@@ -8,9 +8,12 @@ class Build_Text {
 	private $_table = '';
 	private $_tbsId = 0;
 	private $_num_gap = -1;
+	private $_num_img = -3;
 	private $_num_gaps = 0;
 	private $_select = '';
 	private $_txt = '';
+	private $_padding = "pad_right";
+	private $_floating = "float-left";
 	
 
 	function __construct($table){
@@ -29,15 +32,39 @@ class Build_Text {
 		$this->_num_gaps = $row['MAX(gap)'];
 	}
 
+	private function get_Images($mysqli, $num) {
+		$result = $mysqli->query("SELECT img FROM $this->_table WHERE textbausteinID = $this->_tbsId AND imgID = $num");
+		$row = $result->fetch_assoc();
+
+		$img = "<img class='$this->_floating $this->_padding' src='{$row['img']}' alt='img$num'>";
+		
+		if($this->_padding == "pad_right") {
+			$this->_padding = "pad_left";
+			$this->_floating = "float-right";
+		} else {
+			$this->_padding = "pad_right";
+			$this->_floating = "float-left";
+		}
+
+		$this->_txt = str_replace("#$num#", $img, $this->_txt);
+	}
+
 
 	private function build_Select($mysqli) {
 		$this->_select = "<select>";
 		$this->_select .= "<option>- ??? -</option>";
 
-		$result = $mysqli->query("SELECT solution FROM $this->_table WHERE gap != $this->_num_gap");
-		while($row = $result->fetch_assoc()) {
-			$this->_select .= "<option>{$row['solution']}</option>";
+		$result = $mysqli->query("SELECT solution FROM $this->_table WHERE gap != $this->_num_gap AND gap != $this->_num_img");
+		while($row = $result->fetch_assoc()) {			
+			$solution[] = $row['solution'];
 		}
+
+		shuffle($solution);
+
+		foreach ($solution as $value) {
+			$this->_select .= "<option>$value</option>";
+		}
+
 		$this->_select .= "</select>";
 	}
 
@@ -54,6 +81,10 @@ class Build_Text {
 
 		for($i = 0; $i <= $this->_num_gaps; $i++) {
 			$this->_txt = str_replace("-$i-", $this->_select, $this->_txt);
+
+			if(strstr($this->_txt, "#$i#") !== false) {
+				$this->get_Images($mysqli, $i);
+			}
 		}
 
 		return $this->_txt;
