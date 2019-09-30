30ef6da762b7d9e9a0b0ac673ace99c44386a23a
Anselmo Gomez <anselmo.eoi@gmail.com>
2019-09-30 12:48:16 +0200

    include icons in createLV

--- modules/createLV.inc.php
+++ modules/createLV.inc.php
@@ -2,7 +2,6 @@
   require_once('./modules/base_url.php');
 	require_once('./includes/config_db_access.php');
 	require_once('./php/addFile.class.php');
-
 	//create the object to access the db
 	$config = config_db_access::getInstance();
 	$mysqli = $config->getConnection();
@@ -19,8 +18,8 @@
 	<hr>
 	<form name="form" id="myForm" method="post" action="<?php BASE_URL . $_SERVER['PHP_SELF'] ?>" enctype="multipart/form-data">
 		<div class="row buttons">
-			<div class="col-3 offset-sm-1">			
-				<button type="button" class="btn btn-default glyphs" onclick="execCmd('bold')">
+			<div class="col-5 offset-sm-1">			
+				<button type="button" class="btn btn-default glyphs" data-title="bold" onclick="execCmd('bold')">
 					<span class="glyphicon glyphicon-bold"></span>
 				</button>
 				<button type="button" class="btn btn-default glyphs" onclick="execCmd('italic')">
@@ -29,6 +28,24 @@
 				<button type="button" class="btn btn-default glyphs" onclick="execCmd('underline')">
 					<span class="glyphicon glyphicon-text-color"></span>
 				</button>
+				<button type="button" class="btn btn-default glyphs" onclick="execCmd('cut')">
+					<span class="glyphicon glyphicon-scissors"></span>
+				</button>
+				<button type="button" class="btn btn-default glyphs" onclick="execCmd('copy')">
+					<span class="glyphicon glyphicon-duplicate"></span>
+				</button>
+				<button type="button" class="btn btn-default glyphs" onclick="execCmd('paste')">
+					<span class="glyphicon glyphicon-copy"></span>
+				</button>
+				<select name="headers" id="headers" onchange="execCmdWithArg('formatBlock', this.value);">
+					<option value="">??</option>
+					<option value="H1">H1</option>
+					<option value="H2">H2</option>
+					<option value="H3">H3</option>
+					<option value="H4">H4</option>
+					<option value="H5">H5</option>
+					<option value="H6">H6</option>
+				</select>
 			</div>
 			<div class="col-4">
 				<button type="button" class="btn btn-default glyphs" onclick="execCmd('justifyLeft')">
@@ -43,18 +60,21 @@
 				<button type="button" class="btn btn-default glyphs" onclick="execCmd('justifyFull')">
 					<span class="glyphicon glyphicon-align-justify"></span>
 				</button>
+				<button type="button" class="btn btn-default glyphs" onclick="execCmd('insertUnorderedList')">
+					<span class="glyphicon glyphicon-list"></span>
+				</button>
 			</div> 
 		</div>
 		<div class="row">
 			<div class="col-10 offset-1">
-				<iframe name='ricextField' class='rchTF' src='' onload='enableEditMode();'></iframe>
+				<!-- <iframe name='richextField' class='rchTF' src='' onload='enableEditMode();'></iframe>  -->
 				<?php
 					if (isset($_POST["build_memory"])) {
 						$data = file_get_contents($_FILES["userFile"]["tmp_name"]);
-						echo "<textarea name='richTextField'>".htmlspecialchars($data)."</textarea>";
-						//echo "<iframe name='richTextField' class='richTF' src='/EOI_moodle/modules/Lorem_ipsum.txt' onload='enableEditMode();'></iframe>";
+						//echo "<textarea name='richTextField'>".htmlspecialchars($data)."</textarea>";
+						echo "<iframe name='richTextField' class='richTF' srcdoc=\"". htmlspecialchars($data) . "\" onload='enableEditMode();'></iframe>";
 					} else {
-						echo "<iframe name='richxtField' class='richTF' src='' onload='enableEditMode();'></iframe>";
+						//echo "<iframe name='richxtField' class='richTF' src='' onload='enableEditMode();'></iframe>";
 					}
 				?>			
 			</div>
@@ -71,12 +91,16 @@
 	</form>
 
 	<script>
-		function enableEditMode() {
-			richTextField.document.designMode = 'On';
-		}
+		richTextField.document.designMode = 'On';
 
+		function enableEditMode() {
+			richTextField.document.designMode = 'on';
+		}
 		function execCmd(command) {
 			richTextField.document.execCommand(command, false, null);
 		}
 
+		function execCmdWithArg(command, arg) {
+			richTextField.document.execCommand(command, false, arg);
+		}
 	</script>
